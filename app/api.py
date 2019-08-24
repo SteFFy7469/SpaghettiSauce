6c710a3470ac9ee2991e0005f1bcff5b6006e35f
SteFFy7469 <esbbchallenger@gmail.com>
2019-08-24 11:08:04 -0600

    dadfa

--- app/api.py
+++ app/api.py
@@ -17,8 +17,8 @@ def start_response(color):
         },
         body=json.dumps({
             "color": color,
-            "headType": "tongue",
-            "tailType": "small-rattle"
+            "headType": "silly",
+            "tailType": "pixel"
         })
     )
 
